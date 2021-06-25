81d29889942c5ae92962d8f1cfc45dff46aeebcd
Antonia Ito <ito.antonia@waterdine.studio>
2021-06-25 14:43:17 +0100

    Add game logic hierarchy.

---
+++ Sources/Flat47PuzzleGame/PuzzleGameLogic.swift
@@ -0,0 +1,21 @@
+//
+//  PuzzleGameLogic.swift
+//  
+//
+//  Created by x414e54 on 17/06/2021.
+//
+
+import Foundation
+import Flat47Game
+
+// This is a free function because OOP sucks monkey ass and I am not perpetuating that BS any longer ^_^/
+@available(iOS 10.0, *)
+func RegisterPuzzleGameScenes(gameLogic: GameLogic) {
+	//RegisterGameLogicScenes()
+	gameLogic.sceneTypes["DatePuzzle"] = DatePuzzleLogic.newScene(gameLogic: gameLogic)
+	gameLogic.sceneTypes["Janken"] = JankenLogic.newScene(gameLogic: gameLogic)
+	gameLogic.sceneTypes["PipePuzzle"] = PipePuzzleLogic.newScene(gameLogic: gameLogic)
+	gameLogic.sceneTypes["SearchPuzzle"] = SearchPuzzleLogic.newScene(gameLogic: gameLogic)
+	gameLogic.sceneTypes["TV"] = TVLogic.newScene(gameLogic: gameLogic)
+	gameLogic.sceneTypes["ZenPuzzle"] = ZenPuzzleLogic.newScene(gameLogic: gameLogic)
+}
